62ae0a8667d89a4c3f65541995994c20d012b8e4
Makiasake <alexandre.maki@hotmail.com>
2021-06-18 11:58:19 -0300

    Descrição inicialização do projeto, implementado navegação entre paginas, melhorias de layout

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node-fabricjs-test-with-pdfjs
 Projeto teste para obter a posição de um polígono em um pdf utilizando a biblioteca PDFjs e FabricJS
+
+Instalar dependencias: npm install
+Rodar projeto: npm start
+
+Está configurado para rodar na porta 3000
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,66 +1,113 @@
-var canvas = new fabric.Canvas("pdfcanvas");
+var canvas = new fabric.Canvas("pdfcanvas"),
+  pageNum = 1,
+  pdfDoc = null,
+  pageRendering = false,
+  file = null,
+  rectExists = false;
 
-document.querySelector("#pdf-upload").addEventListener("change", function(e) {
-    var file = e.target.files[0]
-    
+var rect = new fabric.Rect({
+  left: 100,
+  top: 50,
+  fill: 'lightgreen',
+  width: 200,
+  height: 100,
+  objectCaching: false,
+  stroke: 'lightgreen',
+  strokeWidth: 4,
+  opacity: 0.2
+});
+
+document.querySelector("#pdf-upload").addEventListener("change", function (e) {
+  renderPage(pageNum, e);
+});
+
+function renderPage(num, e) {
+
+  if (e) {
+    file = e.target.files[0]
+
     if (file.type != "application/pdf") {
       console.error(file.name, " arquivo não é pdf")
       return
     }
-  
-    var fileReader = new FileReader();
-  
-    fileReader.onload = function() {
-      var typedarray = new Uint8Array(this.result);
-  
-      PDFJS.getDocument(typedarray).then(function(pdf) {
-        // you can now use *pdf* here
-        console.log("PDF com:  ", pdf.numPages, "paginas.")
-        pdf.getPage(1).then(function(page) {
-          // you can now use *page* here
-          var viewport = page.getViewport(1.0);
-          var canvasEl = document.querySelector("canvas")
-          canvasEl.height = viewport.height;
-          canvasEl.width = viewport.width;
-  
-          page.render({
-            canvasContext: canvasEl.getContext('2d'),
-            viewport: viewport
-          }).then(function() {
-  
-            var bg = canvasEl.toDataURL("image/png");
+  }
 
-            var rect = new fabric.Rect({
-                left: 100,
-                top: 50,
-                fill: 'lightgreen',
-                width: 200,
-                height: 100,
-                objectCaching: false,
-                stroke: 'lightgreen',
-                strokeWidth: 4,
-                opacity: 0.2
-              });
-            
+  var fileReader = new FileReader();
+
+  fileReader.onload = function () {
+    var typedarray = new Uint8Array(this.result);
+
+    PDFJS.getDocument(typedarray).then(function (pdf) {
+
+      pdfDoc = pdf;
+
+      pdf.getPage(num).then(function (page) {
+        // you can now use *page* here
+        var viewport = page.getViewport(1.0);
+        var canvasEl = document.querySelector("canvas")
+        canvasEl.height = viewport.height;
+        canvasEl.width = viewport.width;
+
+        page.render({
+          canvasContext: canvasEl.getContext('2d'),
+          viewport: viewport
+        }).then(function () {
+
+          var bg = canvasEl.toDataURL("image/png");
+          
+          if (!rectExists) {
             canvas.add(rect);
             canvas.setActiveObject(rect);
-
-            fabric.Image.fromURL(bg, function(img) {
-              img.scaleToHeight(viewport.height);
-              canvas.setHeight(viewport.height);
-              canvas.setWidth(viewport.width);
-              canvas.setBackgroundImage(img);
-            });
-            canvas.renderAll();
+            rectExists = true;
+          }
+          
+          fabric.Image.fromURL(bg, function (img) {
+            img.scaleToHeight(viewport.height);
+            canvas.setHeight(viewport.height);
+            canvas.setWidth(viewport.width);
+            canvas.setBackgroundImage(img);
           });
         });
-  
+        document.getElementById('page_num').textContent = num;
       });
-    };
-    fileReader.readAsArrayBuffer(file);
-  });
-
-function checkPosition(){
-    var obj = canvas.getActiveObject();
-    alert("left: " + obj.left + ", top: " + obj.top + ", height: " + obj.height * obj.scaleY + ", width: " + obj.width * obj.scaleX);
+      document.getElementById('page_count').textContent = pdfDoc.numPages;
+    });
+  };
+  fileReader.readAsArrayBuffer(file);
+  canvas.renderAll();
 }
+
+function checkPosition() {
+  var obj = canvas.getActiveObject();
+  alert("left: " + obj.left + 
+        "\ntop: " + obj.top + 
+        "\nheight: " + obj.height * obj.scaleY + 
+        "\nwidth: " + obj.width * obj.scaleX);
+  console.log("left: " + obj.left + ", top: " + obj.top + ", height: " + obj.height * obj.scaleY + ", width: " + obj.width * obj.scaleX);
+}
+
+function queueRenderPage(num) {
+  if (pageRendering) {
+    pageNumPending = num;
+  } else {
+    renderPage(num);
+  }
+}
+
+function onNextPage() {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+}
+document.getElementById('next').addEventListener('click', onNextPage);
+
+function onPrevPage() {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+}
+document.getElementById('prev').addEventListener('click', onPrevPage);
